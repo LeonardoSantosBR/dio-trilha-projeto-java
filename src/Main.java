32d660e4138c03deba79e1092be268b866de33fa
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-01-07 10:54:12 -0300

    fix: tipos de variaveis

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+
+        // entry and exit
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type a number between 1 and 10: ");
+        int nmb = scanner.nextInt();
+
+
+        //  --primitive types--
+        byte age = 10;
+        int population = 1000000;
+        long estelarDistance = 9460730472580L;
+        float price = 19.90f;
+        double wage = 5500.50;
+        String country = "BRAZIL";
+        char letter = 'L';
+        int[] numbers = {10, 12, 123};
     }
 }

12d53a2ff670e6b6e83fa6033d65885d114c8082
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-01-09 11:08:59 -0300

    fix: operadores

---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+    public static void typesVariable(){
+        // ===============================
+        // 1. TIPOS DE VARIAVEIS
+        // ===============================
+        byte age = 10;
+        int population = 1000000;
+        long stellarDistance = 9460730472580L;
+        float price = 19.90f;
+        double wage = 5500.50;
+        String country = "BRAZIL";
+        char letter = 'L';
+
+        System.out.println(age);
+        System.out.println(population);
+        System.out.println(stellarDistance);
+        System.out.println(price);
+        System.out.println(stellarDistance);
+        System.out.println(wage);
+        System.out.println(country);
+        System.out.println(letter);
+    }
+}
